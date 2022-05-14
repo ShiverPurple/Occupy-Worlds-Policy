918a5a45a15f8a8dc9d7e1e40d54e1a07a2aaf19
Wanderson Knight <39245594+WandersonKnight@users.noreply.github.com>
2022-05-14 17:01:17 -0300

    Add files via upload

---
+++ GlobalMoney.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using TMPro;
+using System.IO;
+
+public class GlobalMoney : MonoBehaviour
+{
+
+    #region Initialization / Declaration
+
+    public PlayerData globalMoneyData = new PlayerData();
+
+    [SerializeField] private TextMeshProUGUI moneyDisplay;
+
+    public static double totalMoneyCount = 0d;
+    public static double moneyCount = 0d;
+
+    #endregion
+
+    void Start()
+    {
+        if (File.Exists(Path.Combine(Application.persistentDataPath, "owsave.k")) || File.Exists(Path.Combine(Application.persistentDataPath, "owsavecloud.k")))
+        {
+            globalMoneyData = SaveSystem.LoadPlayer();
+            InputValues(globalMoneyData);
+        }
+    }
+
+    void InputValues(PlayerData data)
+    {
+        moneyCount = data.saveMoneyCount;
+        totalMoneyCount = data.saveTotalMoneyCount;
+    }
+
+    void Update()
+    {
+        moneyDisplay.SetText("<sprite=12>" + MainClickButton.instance.FormatNumber(moneyCount));
+    }
+
+}
---
+++ PerSecond.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+using System;
+using System.IO;
+
+public class PerSecond : MonoBehaviour{
+
+    #region Initialization / Declaration
+
+    public PlayerData perSecondData = new PlayerData();
+
+    [SerializeField] private TextMeshProUGUI perSecondDisplay;
+
+    public static double moneyPerSecond = 1d;
+    private double oldPerSecond = moneyPerSecond;
+    private double oldClick = 0d;
+    private double oldAutoClick = 0d;
+
+    #endregion
+
+    void Start()
+    {
+        if (File.Exists(Path.Combine(Application.persistentDataPath, "owsave.k")) || File.Exists(Path.Combine(Application.persistentDataPath, "owsavecloud.k")))
+        {
+            perSecondData = SaveSystem.LoadPlayer();
+            InputValues(perSecondData);
+        }
+
+        perSecondDisplay.SetText(MainClickButton.instance.FormatNumber(moneyPerSecond + (UpgradeMenu.autoClickValue * (MainClickButton.clickValue / 10d))) + "/s");
+
+        StartPSecondCoroutine();
+    }
+
+    void InputValues(PlayerData data)
+    {
+        moneyPerSecond = data.saveMoneyPerSecond;
+    }
+
+    void Update()
+    {
+        if(oldPerSecond != moneyPerSecond || oldClick != MainClickButton.clickValue || oldAutoClick != UpgradeMenu.autoClickValue)
+        {
+            perSecondDisplay.SetText(MainClickButton.instance.FormatNumber(moneyPerSecond + (UpgradeMenu.autoClickValue * (MainClickButton.clickValue / 10d))) + "/s");
+
+            oldPerSecond = moneyPerSecond;
+            oldClick = MainClickButton.clickValue;
+        }     
+    }
+
+    void StartPSecondCoroutine()
+    {
+        StartCoroutine(PSecond());
+    }
+
+    IEnumerator PSecond()
+    {
+        while(true)
+        {
+            if(moneyPerSecond > 20)
+            {
+                double aux = moneyPerSecond / 20d;
+
+                GlobalMoney.moneyCount += aux;
+                GlobalMoney.totalMoneyCount += aux;
+
+                yield return new WaitForSeconds(0.05f);
+            }
+            else
+            {
+                double time = 1d / moneyPerSecond;
+
+                GlobalMoney.moneyCount += 1d;
+                GlobalMoney.totalMoneyCount += 1d;
+
+                yield return new WaitForSeconds(Convert.ToSingle(time));
+            }
+        }
+    }
+
+}
